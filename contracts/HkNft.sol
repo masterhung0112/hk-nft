f609720171907a5223ad41df512bc04a6ddda860
hungbn <masterhung0112@gmail.com>
2022-01-07 15:28:37 +0700

    Add @openzeppelin/contracts

--- contracts/HkNft.sol
+++ contracts/HkNft.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -15,11 +15,48 @@ contract HkNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Strings for uint256;
     string public baseURI = "";
 
+    struct HkNFTValue {
+        string name;
+        uint256 id;
+        uint256 dna;
+        uint8 level;
+        uint8 rarity;
+    }
+
+    HkNFTValue[] public nfts;
+
+    event NewNFT(address indexed owner, uint256 id, uint256 dna);
+
     constructor() ERC721("HungKnow NFT", "HKNFT") {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, _tokenIdCounter.current());
+    /***
+     * NFT CRUDs
+     */
+    function _mintNFT(string memory _name, uint256 _dna) internal {
+        HkNFTValue memory newNFT = HkNFTValue(_name, _tokenIdCounter.current(), _dna, 1, 50);
+        nfts.push(newNFT);
+        // Mint to the same callee address
+        _safeMint(msg.sender, _tokenIdCounter.current());
         _tokenIdCounter.increment();
+
+        emit NewNFT(msg.sender, newNFT.id, newNFT.dna);
+    }
+
+    function getNft() public view returns(HkNFTValue[] memory) {
+        return nfts;
+    }
+
+    /**
+     * Helpers
+     */
+    function _genRandomDna(string memory _str) internal pure returns(uint256) {
+        uint256 randomNum = uint256(keccak256(abi.encodePacked(_str)));
+        return randomNum % 10**16;
+    }
+
+    function createRandomNft(string memory _name) public {
+        uint256 randomDna = _genRandomDna(_name);
+        _mintNFT(_name, randomDna);
     }
 
     function _burn(uint256 tokenId)
